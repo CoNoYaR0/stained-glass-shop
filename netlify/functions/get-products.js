87e4d9f1c43c1e96d3c6a1795e5ede799f966b50
CoNoYaR0 <khalil.lost@gmail.com>
2025-04-13 20:07:28 +0200

    CLEAN UP GET PROD FTECH JS

--- netlify/functions/get-products.js
+++ netlify/functions/get-products.js
@@ -1,25 +1,26 @@
+
 export async function handler(event) {
-    const DOLIBARR_URL = process.env.DOLIBARR_URL;
-    const DOLIBARR_TOKEN = process.env.DOLIBARR_TOKEN;
-  
-    try {
-      const res = await fetch(`${DOLIBARR_URL}/products`, {
-        headers: {
-          "DOLAPIKEY": DOLIBARR_TOKEN
-        }
-      });
-  
-      const data = await res.json();
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data)
-      };
-    } catch (err) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ message: "Erreur Dolibarr", error: err.message })
-      };
-    }
+  const DOLIBARR_URL = process.env.DOLIBARR_URL;
+  const DOLIBARR_TOKEN = process.env.DOLIBARR_TOKEN;
+
+  try {
+    const res = await fetch(`${DOLIBARR_URL}/products`, {
+      headers: {
+        "DOLAPIKEY": DOLIBARR_TOKEN,
+        "Accept": "application/json"
+      }
+    });
+
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Erreur Dolibarr", error: err.message })
+    };
   }
-  
+}
