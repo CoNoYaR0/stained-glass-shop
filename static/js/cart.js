f82a67e8b4f4fae3eabd70cdfdc70584b13e1583
CoNoYaR0 <khalil.lost@gmail.com>
2025-04-21 15:50:07 +0200

    sync v10

--- static/js/cart.js
+++ static/js/cart.js
@@ -67,22 +67,32 @@ async function loadDolibarrProducts() {
 
       data.products.forEach(product => {
         const productHTML = `
-          <div class="product-card">
-            <img src="${product.image}" alt="${product.name}">
-            <h3>${product.name}</h3>
-            <p>${product.price.toFixed(2)} TND</p>
-            <button 
-              class="add-to-cart" 
-              data-id="${product.id}" 
-              data-name="${product.name}"
-              data-price="${product.price}" 
-              data-image="${product.image}"
-              ${product.stock === 0 ? 'disabled' : ''}
-            >
-              ${product.stock === 0 ? 'Sold Out' : 'Ajouter au panier'}
-            </button>
-          </div>
-        `;
+  <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+    <div class="block">
+      <div class="gallery-overlay">
+        <a href="/produit/${product.id}" class="gallery-popup">
+          <i class="tf-ion-plus-round"></i>
+        </a>
+      </div>
+      <img class="img-fluid" src="${product.image}" alt="${product.name}">
+    </div>
+    <div class="product-info">
+      <h4 class="mb-2">
+        <a href="/produit/${product.id}" class="link-title">${product.name}</a>
+      </h4>
+      <p class="price">${product.price.toFixed(2)} TND</p>
+      <div class="text-center mt-2">
+        <button class="btn btn-sm btn-outline-primary add-to-cart"
+          data-id="${product.id}"
+          data-name="${product.name}"
+          data-price="${product.price}"
+          data-image="${product.image}">
+          ${product.stock === 0 ? 'Sold Out' : 'Ajouter au panier'}
+        </button>
+      </div>
+    </div>
+  </div>
+`;
 
         productsContainer.innerHTML += productHTML;
       });
